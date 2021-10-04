87a903f04d147a56c1f4b9b9918ff1b1affa0803
Gabriela Di Salvatore-Bosshard <gabrieladisalvatore@gmail.com>
2021-10-04 17:46:15 +0100

    added the titel and js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const loveTravel = document.querySelector(".love-travel");
+const titel = document.querySelector(".titel");
+
+window.addEventListener('load', (event) => {
+    titel.style.opacity='1';
+    loveTravel.style.opacity='1';
+});
